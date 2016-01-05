70610d920e6e057e385b60284bd9c14415dd9c3f
Takeru Chuganji <takeru@hoppenichu.com>
2016-01-05 23:29:11 +0900

    Add Carthage Support

---
+++ Device/Device.h
@@ -0,0 +1,19 @@
+//
+//  Device.h
+//  Device
+//
+//  Created by Takeru Chuganji on 1/5/16.
+//  Copyright (c) 2016 Ekhoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Device.
+FOUNDATION_EXPORT double DeviceVersionNumber;
+
+//! Project version string for Device.
+FOUNDATION_EXPORT const unsigned char DeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Device/PublicHeader.h>
+
+
