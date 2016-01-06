ba08357982272707d08d2931d418f48b054d1bbf
Seth Hein <me@reallyseth.com>
2016-01-06 08:18:45 -0600

    Adding Carthage Support
    - added a framework target
    - shared the scheme

---
+++ Device/Device.h
@@ -0,0 +1,19 @@
+//
+//  Device.h
+//  Device
+//
+//  Created by Seth Hein on 1/6/16.
+//  Copyright © 2016 Ekhoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Device.
+FOUNDATION_EXPORT double DeviceVersionNumber;
+
+//! Project version string for Device.
+FOUNDATION_EXPORT const unsigned char DeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Device/PublicHeader.h>
+
+
