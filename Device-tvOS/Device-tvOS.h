d6b0dbc9bce35be50d1ee8a9ead097c80cd2fac1
toshi0383 <t.suzuki326@gmail.com>
2016-05-19 00:56:55 +0900

    Add Framework and ExampleApp targets for tvOS

---
+++ Device-tvOS/Device-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Device-tvOS.h
+//  Device-tvOS
+//
+//  Created by toshi0383 on 5/19/16.
+//  Copyright © 2016 Ekhoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Device-tvOS.
+FOUNDATION_EXPORT double Device_tvOSVersionNumber;
+
+//! Project version string for Device-tvOS.
+FOUNDATION_EXPORT const unsigned char Device_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Device_tvOS/PublicHeader.h>
+
+
