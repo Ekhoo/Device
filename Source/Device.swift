2de1950d261d98669b03b91a9b850a63a9be6d28
Piotr Sochalewski <sochalewski@gmail.com>
2016-10-22 19:58:43 +0200

    getType(code:) in safe way

--- Source/Device.swift
+++ Source/Device.swift
@@ -65,41 +65,16 @@ open class Device {
     static fileprivate func getType(code: String) -> Type {
         let versionCode = Device.getVersionCode()
         
-        switch versionCode {
-            case "iPhone3,1", "iPhone3,2", "iPhone3,3",
-            "iPhone4,1", "iPhone4,2", "iPhone4,3",
-            "iPhone5,1", "iPhone5,2",
-            "iPhone5,3", "iPhone5,4",
-            "iPhone6,1", "iPhone6,2",
-            "iPhone7,2",
-            "iPhone7,1",
-            "iPhone8,1",
-            "iPhone8,2",
-            "iPhone8,4",
-            "iPhone9,1", "iPhone9,3",
-            "iPhone9,2", "iPhone9,4":                       return Type.iPhone
-
-            case "iPad1,1",
-            "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4",
-            "iPad3,1", "iPad3,2", "iPad3,3",
-            "iPad3,4", "iPad3,5", "iPad3,6",
-            "iPad4,1", "iPad4,2", "iPad4,3",
-            "iPad5,3", "iPad5,4",
-            "iPad2,5", "iPad2,6", "iPad2,7",
-            "iPad4,4", "iPad4,5", "iPad4,6",
-            "iPad4,7", "iPad4,8", "iPad4,9",
-            "iPad5,1", "iPad5,2",
-            "iPad6,3", "iPad6,4", "iPad6,7", "iPad6,8":     return Type.iPad
-
-            case "iPod1,1",
-            "iPod2,1",
-            "iPod3,1",
-            "iPod4,1",
-            "iPod5,1",
-            "iPod7,1":
-                                                            return Type.iPod
-            case "i386", "x86_64":                          return Type.Simulator
-            default:                                        return Type.Unknown
+        if versionCode.contains("iPhone") {
+            return Type.iPhone
+        } else if versionCode.contains("iPad") {
+            return Type.iPad
+        } else if versionCode.contains("iPod") {
+            return Type.iPod
+        } else if versionCode == "i386" || versionCode == "x86_64" {
+            return Type.Simulator
+        } else {
+            return Type.Unknown
         }
     }
 
