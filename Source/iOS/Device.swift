893160bbe1ea70dd3effafb9b1a8f152d049a4c6
Eliah Snakin <ilya@nikans.com>
2018-09-18 16:14:29 +0300

    updated new iphones model versions

--- Source/iOS/Device.swift
+++ Source/iOS/Device.swift
@@ -36,9 +36,9 @@ open class Device {
             case "iPhone10,1", "iPhone10,4":                 return .iPhone8
             case "iPhone10,2", "iPhone10,5":                 return .iPhone8Plus
             case "iPhone10,3", "iPhone10,6":                 return .iPhoneX
-            case "iPhone11,2": /* rumoured */                return .iPhoneXS
-            case "iPhone11,4": /* rumoured */                return .iphoneXS_Max
-            case "iPhone11,8": /* rumoured */                return .iphoneXR
+            case "iPhone11,2":                               return .iPhoneXS
+            case "iPhone11,4", "iPhone11,6":                 return .iphoneXS_Max
+            case "iPhone11,8":                               return .iphoneXR
 
             
             /*** iPad ***/
