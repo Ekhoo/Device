6506dbb4d7213565559740c578ff81b0f7d8f533
417-72KI <417.72ki@gmail.com>
2021-11-04 14:58:54 +0900

    add iPhone 13s

--- Source/iOS/Device.swift
+++ Source/iOS/Device.swift
@@ -51,6 +51,10 @@ open class Device {
             case "iPhone13,2":                               return .iPhone12
             case "iPhone13,3":                               return .iPhone12Pro
             case "iPhone13,4":                               return .iPhone12Pro_Max
+            case "iPhone14,4":                               return .iPhone13Mini
+            case "iPhone14,5":                               return .iPhone13
+            case "iPhone14,2":                               return .iPhone13Pro
+            case "iPhone14,3":                               return .iPhone13Pro_Max
 
             /*** iPad ***/
             case "iPad1,1", "iPad1,2":                       return .iPad1
